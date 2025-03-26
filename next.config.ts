23795b8017f3df881b9c7dba511b67cc14394607
Vitali Lavau <v.lavov@imedia.by>
2025-03-26 18:29:13 +0100

    master deploy SPA gitHub pages fix 26.03.25

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
   basePath: '/ecosystem-alpha-test',
-  assetPrefix: '/ecosystem-alpha-test/',
-
+  assetPrefix: '/ecosystem-alpha-test',
   images: {
     unoptimized: true,
     remotePatterns: [
